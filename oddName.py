d8c3d0006abd9e916ea81c850a26ec2a81e537ab
saniovincent97 <svinc26@hotmail.com>
2016-09-30 13:06:19 +1000

    Update to last few weeks of work

--- oddName.py
+++ oddName.py
@@ -1,11 +1,13 @@
-name_input = input("Please enter your name")
-name_length = len(name_input)
-while name_length == 0:
-    name_input = input("Please enter a valid name")
+def main():
+    name_input = input("Please enter your name")
     name_length = len(name_input)
-for letter in range(1, name_length,2):
-    print(name_input[letter], end="")
+    while name_length == 0:
+        name_input = input("Please enter a valid name")
+        name_length = len(name_input)
+    for letter in range(1, name_length, 2):
+        print(name_input[letter], end="")
 
+main()
 
 
 
